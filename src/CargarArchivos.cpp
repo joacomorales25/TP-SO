911140dd05b22ef6ecce9b19dbc018e7e8e69195
Morales Pessacq Joaquin <mjoaquin@dc.uba.ar>
2023-06-05 18:37:49 -0300

    agrego todo

---
+++ src/CargarArchivos.cpp
@@ -0,0 +1,83 @@
+#ifndef CHM_CPP
+#define CHM_CPP
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <thread>
+#include <pthread.h>
+
+#include "CargarArchivos.hpp"
+
+using namespace std;
+int cargarArchivo(
+    HashMapConcurrente &hashMap,
+    std::string filePath
+) {
+    std::fstream file;
+    int cant = 0;
+    std::string palabraActual;
+
+    // Abro el archivo.
+    file.open(filePath, file.in);
+    if (!file.is_open()) {
+        std::cerr << "Error al abrir el archivo '" << filePath << "'" << std::endl;
+        return -1;
+    }
+    while (file >> palabraActual) {
+        // Completar (Ejercicio 4)
+        hashMap.incrementar(palabraActual);
+        cant++;
+    }
+    // Cierro el archivo.
+    if (!file.eof()) {
+        std::cerr << "Error al leer el archivo" << std::endl;
+        file.close();
+        return -1;
+    }
+    file.close();
+    return cant;
+}
+
+void cargarArchivoThread(int principio, int fin, HashMapConcurrente& hashMap, vector<std::string>& filePaths)
+{
+    for (unsigned int index = principio; index < fin; index++) {
+        cargarArchivo(hashMap, filePaths[index]);
+    }
+}
+
+void cargarMultiplesArchivos(
+    HashMapConcurrente &hashMap,
+    unsigned int cantThreads,
+    std::vector<std::string> filePaths
+) {
+    // Completar (Ejercicio 4)
+    unsigned int cantFilePathsPorThread = filePaths.size() / cantThreads;
+    unsigned int resto = filePaths.size() % cantThreads;
+
+    unsigned int principio = 0;
+    unsigned int fin = cantFilePathsPorThread;
+
+    std::vector<thread> threads;
+    for (unsigned int i = 0; i < cantThreads; i++) {
+        if (resto > 0)
+        {
+            fin++;
+            resto--;
+        }
+
+        threads.emplace_back(thread(cargarArchivoThread, principio, fin, hashMap, std::ref(filePaths)));
+
+        principio = fin;    // Iteramos hasta fin (no inclusive) en cargarArchivoThread
+        fin = (fin + cantFilePathsPorThread > filePaths.size()) ? filePaths.size() : fin + cantFilePathsPorThread;
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+}
+
+
+
+#endif
---
+++ src/HashMapConcurrente.cpp
@@ -0,0 +1,155 @@
+#ifndef CHM_CPP
+#define CHM_CPP
+
+#include <iostream>
+#include <fstream>
+#include <pthread.h>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include "HashMapConcurrente.hpp"
+
+using namespace std;
+
+std::mutex mutexes[HashMapConcurrente::cantLetras];
+
+HashMapConcurrente::HashMapConcurrente() {
+    for (unsigned int i = 0; i < HashMapConcurrente::cantLetras; i++) {
+        tabla[i] = new ListaAtomica<hashMapPair>();
+    }
+}
+
+unsigned int HashMapConcurrente::hashIndex(std::string clave) {
+    return (unsigned int)(clave[0] - 'a');
+}
+
+void HashMapConcurrente::incrementar(std::string clave) {
+    // Completar (Ejercicio 2)
+    unsigned int index = hashIndex(clave);
+    ListaAtomica<hashMapPair>* lista = tabla[index];
+    mutexes[index].lock();
+    for(unsigned int i = 0; i < lista->longitud(); i++)
+        if (lista->iesimo(i).first == clave) {
+            lista->iesimo(i).second++;
+            mutexes[index].unlock();
+            return;
+        }
+    hashMapPair *par = new hashMapPair();
+    par->first = clave;
+    par->second = 1;
+    lista->insertar(*par);
+    mutexes[index].unlock();
+    }
+
+
+std::vector<std::string> HashMapConcurrente::claves() {
+    // Completar (Ejercicio 2)
+    std::vector<std::string> claves;
+    for (unsigned int index = 0; index < cantLetras; index++) {
+        auto it = tabla[index]->crearIt();
+        while (it.haySiguiente()) {
+            claves.push_back(it.siguiente().first);
+            it.avanzar();
+        }
+    }
+    return claves;
+}
+
+unsigned int HashMapConcurrente::valor(std::string clave) {
+    // Completar (Ejercicio 2)
+    unsigned int index = hashIndex(clave);
+    ListaAtomica<hashMapPair>* lista = tabla[index];
+    for (unsigned int i = 0; i < lista->longitud(); i++) {
+        if (lista->iesimo(i).first == clave) {
+            return lista->iesimo(i).second;
+        }
+    }
+
+    return 0;
+}
+
+hashMapPair HashMapConcurrente::maximo() {
+    hashMapPair *max = new hashMapPair();
+    max->second = 0;
+
+    for(unsigned int i = 0; i < cantLetras; i++) mutexes[i].lock();
+
+    for (unsigned int index = 0; index < HashMapConcurrente::cantLetras; index++) {
+        auto it = tabla[index]->crearIt();
+        while (it.haySiguiente()){
+
+            if (it.siguiente().second > max->second) {
+                max->first = it.siguiente().first;
+                max->second = it.siguiente().second;
+            }
+            it.avanzar();
+        }
+    }
+
+    for(int i = 0; i < cantLetras; i++) mutexes[i].unlock();
+
+    return *max;
+}
+
+void buscarMaximoThread(int inicio, int fin, hashMapPair *max, ListaAtomica<hashMapPair>* tabla[]) {
+    for (unsigned int index = inicio; index < fin; index++) {
+        auto it = tabla[index]->crearIt();
+        while (it.haySiguiente())
+        {
+            if (it.siguiente().second > max->second) {
+                max->first = it.siguiente().first;
+                max->second = it.siguiente().second;
+            }
+
+            it.avanzar();
+        }
+    }
+}
+
+hashMapPair HashMapConcurrente::maximoParalelo(unsigned int cantThreads) {
+    // Completar (Ejercicio 3)
+    unsigned int cantListasPorThread = cantLetras / cantThreads;
+    unsigned int resto = cantLetras % cantThreads;
+
+    for(unsigned int i = 0; i < cantLetras; i++) mutexes[i].lock();
+
+    unsigned int principio = 0;
+    unsigned int fin = cantListasPorThread;
+
+    vector<thread> threads;
+    hashMapPair maximos[cantThreads];
+    for (unsigned int i = 0; i < cantThreads; i++) {
+        if (resto > 0)
+        {
+            fin++;
+            resto--;
+        }
+
+        threads.emplace_back(thread(buscarMaximoThread, principio, fin, &maximos[i], this->tabla));
+
+        principio = fin;    // Iteramos hasta fin (no inclusive) en buscarMaximoThread
+        fin = (fin + cantListasPorThread > cantLetras) ? cantLetras : fin + cantListasPorThread;
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    hashMapPair max = maximos[0];
+
+    for (unsigned int i = 0; i < cantThreads; i++) {
+        if (maximos[i].second > max.second) {
+            max.first = maximos[i].first;
+            max.second = maximos[i].second;
+        }
+    }
+
+    for(unsigned int i = 0; i < cantLetras; i++) mutexes[i].unlock();
+
+    return max;
+}
+
+
+#endif
